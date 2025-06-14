6914a7f1230b8c52a699d3ebc6841bb9634ae95a
Arojeet Talukdar <arojeettaluckdar@gmail.com>
2025-06-14 19:11:46 +0530

    Add calculator1.py with basic arithmetic functions ✨

---
+++ Code With Harry/100 Days Python/calculator1.py
@@ -0,0 +1,14 @@
+"""
+Author: Arojeet Talukdar  
+Date: June 14, 2025
+"""
+a = input("Please Enter First Number:")
+b = input("Please Enter Second Number:")
+
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) / int(b))
+print(int(a) // int(b))
+print(int(a) % int(b))
+print(int(a) ** int(b)) 
